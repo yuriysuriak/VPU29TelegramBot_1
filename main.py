0060988f8107c89b5720b7eb24eea0b9529b2321
Nazar Parkasevych <parkasevychnazarij@gmail.com>
2024-04-04 15:26:14 +0300

    додали поширення геолокації

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import logging
 
 from dotenv import load_dotenv
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
 load_dotenv()
 
@@ -16,7 +16,28 @@ logging.basicConfig(
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    keyboard = [
+        [KeyboardButton('Share my location', request_location=True)],
+        ]
+
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Im a bot, please talk to me!',
+        reply_markup=reply_markup
+    )
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+async def location(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    lat = update.message.location.latitude
+    lon = update.message.location.longitude
+
+    await update.message.reply_text(f'lat = {lat}, lon = {lon}')
+
 
 
 if __name__ == '__main__':
@@ -24,5 +45,11 @@ if __name__ == '__main__':
 
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+    hello_handler = CommandHandler('hello', hello)
+    application.add_handler(hello_handler)
+
+    location_handler = MessageHandler(filters.LOCATION, location)
+    application.add_handler(location_handler)
+
 
     application.run_polling()
